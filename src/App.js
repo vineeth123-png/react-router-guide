5427e7a7cc93c263a5f9cfa878a8005cac8a1f35
Vineeth Duvvuri <vineethdvs123@gmail.com>
2021-06-04 10:52:43 +0530

    adding router components for routing in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./index.css"
 
-function App() {
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router>
+    <main>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" exact render={Home} />
+        <Route path="/about" render={About} />
+        <Route path="/contact" render={Contact} />
+      </Switch>
+    </main>
+</Router>
   );
 }
 
-export default App;
+const Home = () => (
+  <Fragment>
+    <h1>Home</h1>
+  </Fragment>
+  );
+
+  const About = () => (
+    <Fragment>
+      <h1>About</h1>
+    </Fragment>
+    );
+  
+  const Contact = () => (
+    <Fragment>
+      <h1>Contact</h1>
+    </Fragment>
+    );
